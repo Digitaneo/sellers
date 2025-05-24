0a0a6aba029eab0b523a9f563e0455ccb778e22a
Digitaneo <152671435+Digitaneo@users.noreply.github.com>
2025-05-24 21:20:27 +0100

    Add files via upload

---
+++ Saab/JS/slick.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#mainSlider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        rtl: true
+    });
+});
